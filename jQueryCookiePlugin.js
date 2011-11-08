cd8560539a2efb17da9568c0257298af55dc3fd6
phoetry <anymango@gmail.com>
2011-11-08 23:20:59 +0800

    By Phoetry

--- jQueryCookiePlugin.js
+++ jQueryCookiePlugin.js
@@ -4,16 +4,16 @@
   * @Author: Phoetry (http://phoetry.me)
   * @Url: http://phoetry.me/archives/jquery-cookie.html
   **/
-~function(doc,$){
+~function(doc){
 $.cookie=function(key,val,opt){
 	if(!key)return doc.cookie;
-	if('string'!=typeof(val)&&'number'!=typeof(val))
+	if('string'!=typeof val&&'number'!=typeof val)
 	return new RegExp('(?:^|; )'+key+'=([^;]*)').test(doc.cookie)?decodeURIComponent(RegExp['$1']):'No matching items.';
-	var cookie=key+'='+encodeURIComponent(val),s,date=new Date();
-	opt=$.extend({expires:30},opt);
-	date.setDate(date.getDate()+(val===''?-1:opt.expires));
+	opt=opt||{};
+	var s,date=new Date(),cookie=key+'='+encodeURIComponent(val);
+	date.setDate(date.getDate()+(val!==''?opt.expires||30:-1));
 	opt.expires=date.toUTCString();
-	for(s in opt)opt[s]&&(cookie+=';'+s+'='+opt[s]);
+	for(s in opt)opt.hasOwnProperty(s)&&opt[s]&&(cookie+=';'+s+'='+opt[s]);
 	doc.cookie=cookie;
 }
-}(document,jQuery);
+}(document);
